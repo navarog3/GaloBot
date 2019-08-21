a9c4a4073e1ca5af94b3934fdc905da7b2fd276c
navarog3 <bspenciner@gmail.com>
2019-08-21 11:15:13 -0400

    Fixing vulnerability

--- bot.js
+++ bot.js
@@ -1,15 +1,18 @@
+// require the discord.js module
 const Discord = require('discord.js');
+// create a new Discord client
 const client = new Discord.Client();
-const auth = require('./auth.json');
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+client.on('message', message => {
+	console.log(message.content);
 });
 
-client.login(auth.token);
+// login to Discord with your app's token
+client.login('your-token-goes-here');
