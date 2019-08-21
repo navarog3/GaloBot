0151a83c7907be69493f797ad1503f201f475cf0
navarog3 <bspenciner@gmail.com>
2019-08-21 13:04:51 -0400

    Mosre testing

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@
 const Discord = require('discord.js');
 // create a new Discord client
 const client = new Discord.Client();
+//get prefix and token
+const {prefix, token} = require('./config.json');
 
 
 // when the client is ready, run this code
@@ -11,8 +13,18 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	console.log(message.content);
+  //if message doesn't contain the prefix or was sent by a bot, ignore it
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  //create args which slices off the prefix and splits it into an array separated by spaces
+  const args = message.content.slice(prefix.length).split(/ +/);
+  //pulls the first item in the array args (removes it from args)
+  const command = args.shift().toLowerCase();
+
+	if (message.content === `${prefix}ping`) {
+    // send back "pong" to the channel the message was sent in
+    message.channel.send('pong');
+  }
 });
 
 // login to Discord with your app's token
-client.login('your-token-goes-here');
+client.login(token);
