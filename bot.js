e983bbe88818f2617438c78cbea6b2ce3f41f824
navarog3 <bspenciner@gmail.com>
2019-08-23 10:55:46 -0400

    Code cleanup and added

--- bot.js
+++ bot.js
@@ -20,15 +20,29 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
+  const commandName = args.shift().toLowerCase();
 
-  if (!client.commands.has(command)) return;
+  if (!client.commands.has(commandName)) {
+    message.reply('No such command exists.');
+  };
+
+  const command = client.commands.get(commandName);
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}.`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
 
   try {
-    client.commands.get(command).execute(message, args);
+    command.execute(message, args);
   } catch (error) {
     console.error(error);
-    message.reply('there was an error trying to execute that command.');
+    message.reply('There was an error trying to execute that command.');
   }
 });
 
--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,9 @@
 module.exports = {
-    name: 'ping',
-    description: 'Ping pong',
-    execute(message, args) {
-        message.channel.send('pong');
-    },
+  name: 'ping',
+  description: 'Ping pong',
+  args: false,
+  usage: '',
+  execute(message, args) {
+    message.channel.send('pong');
+  },
 };
