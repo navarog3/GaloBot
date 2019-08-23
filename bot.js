2d0d3a0ac91dfe22475b3f8fab03a4b222ef065b
navarog3 <bspenciner@gmail.com>
2019-08-23 10:30:25 -0400

    Dynamic command handling

--- bot.js
+++ bot.js
@@ -1,30 +1,35 @@
-// require the discord.js module
+const fs = require('fs');
 const Discord = require('discord.js');
-// create a new Discord client
-const client = new Discord.Client();
-//get prefix and token
 const {prefix, token} = require('./config.json');
 
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-  //if message doesn't contain the prefix or was sent by a bot, ignore it
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-  //create args which slices off the prefix and splits it into an array separated by spaces
+
   const args = message.content.slice(prefix.length).split(/ +/);
-  //pulls the first item in the array args (removes it from args)
   const command = args.shift().toLowerCase();
 
-	if (message.content === `${prefix}ping`) {
-    // send back "pong" to the channel the message was sent in
-    message.channel.send('pong');
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command.');
   }
 });
 
-// login to Discord with your app's token
 client.login(token);
