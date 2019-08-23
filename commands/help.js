83c191f5770d832d887084c1815344ff3f9f700f
navarog3 <bspenciner@gmail.com>
2019-08-23 12:18:38 -0400

    Added files for new commands, just skeleton code for now

--- commands/help.js
+++ commands/help.js
@@ -32,7 +32,6 @@ module.exports = {
 
   data.push(`**Name:** ${command.name}`);
 
-  if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
   if (command.description) data.push(`**Description:** ${command.description}`);
   if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
---
+++ commands/make-ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'make-ping',
+    description: 'Creates a ping with the given name (no spaces pls).',
+    usage: '[ping_name]',
+    args: true,
+    execute(message, args) {
+      //message.channel.send('pong');
+        
+    },
+  };
