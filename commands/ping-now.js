e7d2ba97cf39a2083e5631429a6bc8b35d2d032d
navarog3 <bspenciner@gmail.com>
2019-08-23 23:35:47 -0400

    -now works

---
+++ commands/ping-now.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'ping-now',
+  description: 'Forces the given ping to happen now, regardless of time left.',
+  usage: '[ping_name]',
+  args: true,
+    execute(message, args) {
+        const fileName = `./pings/${args[0]}.json`;
+        const rawData = fs.readFileSync(fileName);
+        const ping = JSON.parse(rawData);
+        
+        message.reply(`${args[0]}:\n ${ping.msg}`);
+    },
+};
